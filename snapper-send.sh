bd59b31ce56c9098a70c58c95c331d8f0718b8be
longranger <longranger.406@gmail.com>
2015-08-02 15:46:26 -0700

    added ability to specify exisitng new snapshot

--- snapper-send.sh
+++ snapper-send.sh
@@ -2,12 +2,10 @@
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 
 nPingMSlimit=3
+sDescription="snapper-send-current"
+sDescriptionOld="snapper-send-old"
 
 # Snapper config must be set with the following:
-
-# Keep one number snapshot... created by / for backup to other end,
-# and kept to compare to next creation / send
-# NUMBER_LIMIT="1"
 
 #set -o nounset
 #set -o errexit
@@ -20,7 +18,7 @@ usage() {
     cat <<EOF
 Usage: 
 
-$sProg [-t|--target hostname] [-e|--email email@address.com] [--init] snapper_config /dest/path
+$sProg [-t|--target hostname] [-s|--snapshot ssNum] [-e|--email email@address.com] [--init] snapper_config /dest/path
 
 Arguments:
 	snapper_config Must match a snapper config in /root/snapper/configs
@@ -28,9 +26,11 @@ Arguments:
 
 Options:
 	-t|--target    Target hostname (if transferring via ssh)
+	-s|--snapshot  Existing snapshot (number) to send. Without this
+	               argument, a new snapshot is created to send.
 	-e|--email     Email address to send any errors to
-	   --init      Complete backup. This creates the first number snapshot on
-	               source and sends it to target.
+	   --init      Complete backup. This creates the first snapper-send
+	               snapshot on source and sends it to target.
 
 EOF
 	exit 0
@@ -80,10 +80,12 @@ trap 'die "Error line $LINENO with status $?"' ERR
 ##
 bInit=""
 sEmailAddress=""
-TEMP=$(getopt --longoptions help,usage,init,target:,email: -o h,t:,e: -- "$@") || usage
+TEMP=$(getopt --longoptions help,usage,init,target:,snapshot:,email: -o h,t:,s:,e: -- "$@") || usage
 sTargetHost=localhost
+nNewSnapshot=""
 sSCPtarget=""
 sSSH=""
+bExistingSS=false
 
 # getopt quotes arguments with ' We use eval to get rid of that
 eval set -- $TEMP
@@ -101,6 +103,13 @@ do
 			sTargetHost=$1
 			sSCPtarget="$sTargetHost:"
 			sSSH="ssh $sTargetHost"
+			shift
+			;;
+
+		--snapshot|-s)
+			shift
+			bExistingSS=true
+			nNewSnapshot=$1
 			shift
 			;;
 
@@ -129,7 +138,8 @@ done
 
 [[ $# != 2 ]] && usage
 sConfig=$1
-grep SUBVOLUME /etc/snapper/configs/$sConfig > /dev/null || die "snapper config \"$sConfig\" does not exist or is missing its subvolume path"
+grep SUBVOLUME /etc/snapper/configs/$sConfig > /dev/null || \
+	die "snapper config \"$sConfig\" does not exist or is missing its subvolume path"
 sPathCfg=$(sudo snapper -c $sConfig get-config | grep SUBVOLUME | cut -d '|' -f 2 | trim)
 sPathDestRoot=$2/.snapshots
 
@@ -138,53 +148,73 @@ sPathDestRoot=$2/.snapshots
 # Sanity checks
 ##
 
-# Check number of existing number snapshots
-echo 'nCntNumberSnapshots=$(snapper -c '$sConfig' ls | grep number | wc -l)'
-nCntNumberSnapshots=$(snapper -c $sConfig ls | grep number | wc -l)
-echo count of number snapshots: $nCntNumberSnapshots
+# Check number of existing snapshots with the matching description
+nCntNumberSnapshots=$(snapper -c $sConfig ls | grep "$sDescription" | wc -l)
 if [[ -n "$bInit" ]]; then
-	test $nCntNumberSnapshots -eq 0 || die "Can not init as $nCntNumberSnapshots number snapshots already exist on $HOSTNAME"
+	test $nCntNumberSnapshots -eq 0 || \
+		die "Can not init as $nCntNumberSnapshots number snapshots already exist on $HOSTNAME"
 else
 	# Must be exactly 1 existing number snapshot
-	test $nCntNumberSnapshots -eq 1 || die "Need exactly 1 number snapshots on $HOSTNAME, have $nCntNumberSnapshots"
-	nOldSnapshot=$(snapper -c $sConfig ls | grep number | cut -d '|' -f 2 | trim)
-	echo old number Snaphot = $nOldSnapshot
+	test $nCntNumberSnapshots -eq 1 || \
+		die "Need exactly 1 number snapshots on $HOSTNAME, have $nCntNumberSnapshots"
+	nOldSnapshot=$(snapper -c $sConfig ls | grep "$sDescription" | cut -d '|' -f 2 | trim)
+	echo old snapper-send Snapshot = $nOldSnapshot
 fi
 
 # Check if target is close enough to sync to
 # Not sure how to check throughut, but ping < a few average should be lan
-ping -c1 $sTargetHost &>/dev/null || die "Unable to reach destination $sTargetHost"
+ping -c1 $sTargetHost &>/dev/null || \
+	die "Unable to reach destination $sTargetHost"
 nPingMS=$(ping -qc9 -i 0.2 $sTargetHost | grep rtt | cut -d '/' -f 5 | cut -d '.' -f 1)
-test $nPingMS -lt $nPingMSlimit || die "Destination $sTargetHost does not seem to be on LAN"
+test $nPingMS -lt $nPingMSlimit || \
+	die "Destination $sTargetHost does not seem to be on LAN"
 
 # Ensure /dest/path exists (on target)
-$sSSH test -d "$sPathDestRoot/" || die "$sPathDestRoot not a directory (on $sTargetHost). Likely should be a subvolume. Create it first. (this is normal for init...)"
+$sSSH test -d "$sPathDestRoot/" || \
+	die "$sPathDestRoot not a directory (on $sTargetHost). Likely should be a subvolume. Create it first. (this is normal for init...)"
 
 
 ##
 # Really doing this
 ##
-# Create local number snapshot via snapper, saving snapshot number
-nNewSnapshot=$(snapper -c $sConfig create -p -c number)
+if [[ -n "$nNewSnapshot" ]]; then
+	# This snapshot number was passed in.
+	# Check it exists, and is newer than the old snapshot
+	if [ $nOldSnapshot -ge $nNewSnapshot ]; then
+		die "New Snapshot ($nNewSnapshot) must be newer than Old Snapshot ($nOldSnapshot)"
+	fi
+	snapper -c $sConfig status $nOldSnapshot..$nNewSnapshot >/dev/null || dir "New Snapshot ($nNewSnapshot) does not exist"
+else
+	# Create local number snapshot via snapper, saving snapshot number
+	nNewSnapshot=$(snapper -c $sConfig create -p -c number -d "$sDescription")
+fi
+
+echo New snapper-send Snapshot = $nNewSnapshot
+
 # Create the actual snapshot directory
 sPathDest=$sPathDestRoot/$nNewSnapshot
 $sSSH mkdir $sPathDest
 
-# Send difference between that snapshot and the previous number one
 #TODO - confirm if this works correctly with non-root subvolumes (trailing slash)
 sPathNew=$sPathCfg/.snapshots/$nNewSnapshot/snapshot
 fNewInfo=$sPathCfg/.snapshots/$nNewSnapshot/info.xml
-if [[ -n "$bInit" ]]; then
-	btrfs send $sPathNew | $sSSH btrfs receive "$sPathDest/"
-#	echo "btrfs send $sPathNew | $sSSH btrfs receive \"$sPathDest/\""
-else
-	sPathOld=$sPathCfg/.snapshots/$nOldSnapshot/snapshot
-	btrfs send -p $sPathOld $sPathNew | $sSSH btrfs receive "$sPathDest/"
-#	echo 'btrfs send -p '$sPathOld $sPathNew' | '$sSSH' btrfs receive "'$sPathDest/'"'
-#	echo "btrfs send -p $sPathOld $sPathNew | $sSSH btrfs receive \"$sPathDest/\""
-fi
+
 # Copy over the info.xml from the new snapshot
 scp $fNewInfo $sSCPtarget"$sPathDest/"
 
-# On successful send, run the number cleanup
-snapper -c $sConfig cleanup number
+if [[ -n "$bInit" ]]; then
+	btrfs send $sPathNew | $sSSH btrfs receive "$sPathDest/" || \
+		die "btrfs send failed from $sPathNew to $sSSH:$sPathDest"
+else
+	sPathOld=$sPathCfg/.snapshots/$nOldSnapshot/snapshot
+	btrfs send -p $sPathOld $sPathNew | $sSSH btrfs receive "$sPathDest/" || \
+		die "btrfs send failed from $sPathNew - $sPathOld to $sSSH:$sPathDest"
+
+	# On successful send, rename the old snapshot
+	snapper -c $sConfig modify -d "$sDescriptionOld" $nOldSnapshot
+
+	#TODO - if copying an existing snapshot, change it's description.
+	if [[ $bExistingSS ]]; then
+		snapper -c $sConfig  modify -d "$sDescription" $nNewSnapshot
+	fi
+fi
